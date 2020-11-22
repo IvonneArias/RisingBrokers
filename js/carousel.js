99e2c09ddca8ade4bd4d2b0460952083c59c2555
IvonneArias <54588701+IvonneArias@users.noreply.github.com>
2020-11-22 13:51:33 -0500

    Website Creation

---
+++ js/carousel.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const elementsCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementsCarousel, {
+        duration: 150
+    });
+});
